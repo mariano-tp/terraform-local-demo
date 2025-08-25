1111707d1679fd7aa318577ddea804d57ec1b642
mariano-tp <228102224+mariano-tp@users.noreply.github.com>
2025-08-25 18:13:26 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,7 +1,14 @@
 terraform {
   required_version = ">= 1.6"
+
   required_providers {
-    random = { source = "hashicorp/random", version = ">= 3.6.0" }
-    local  = { source = "hashicorp/local",  version = ">= 2.4.0" }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.5"
+    }
   }
 }
